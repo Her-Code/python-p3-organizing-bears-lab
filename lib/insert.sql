8662a20d0ee104a1556308bc80759d7c7ee51ef2
+Sharon <+wanjiku.kahira@gmail.com>
2024-09-15 23:00:05 +0300

    data and database

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Mr. Chocolate', 3,'M','black','friendly',0);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Rowdy', 5,'F','brown','tempered',1);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Tabitha', 7,'M','white','calm',1);
+INSERT INTO bears(name, age, sex,color,temperament,alive) VALUES ('Sergeant Brown', 8,'F','black','playful',1);
+INSERT INTO bears(name, age, sex,color,temperament,alive) VALUES ('Melissa', 6,'M','brown','aggressive',0);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Grinch', 9,'M','white','independent',1);
+INSERT INTO bears(name, age, sex,color,temperament,alive) VALUES ('Wendy', 10,'F','grey','friendly',0);
+INSERT INTO bears(name, age, sex,color,temperament,alive) VALUES (NULL, 2,'F','grey','playful',0);
